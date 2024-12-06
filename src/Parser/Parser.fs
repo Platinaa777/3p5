3f1640fda39311912bff757f9e99bda3b0386f51
Denis Miroshnichenko <miroshnichenkodenis2004@gmail.com>
2024-12-06 21:19:18 +0300

    add initial parser impl

---
+++ src/Parser/Parser.fs
@@ -0,0 +1,139 @@
+﻿namespace Parser
+
+open FParsec
+open System
+
+type Id = string
+
+type Value =
+    | Int of int
+    | Float of float
+    | Bool of bool
+    | Str of string
+    
+type Operator =
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
+    | Mod
+    // comparisons operators
+    | GreaterThan
+    | LessThan
+    | GreaterThanOrEqual
+    | LessThanOrEqual
+    | Equal
+    | NotEqual
+    // condition operators
+    | And
+    | Or
+        
+type Expression =
+    | LiteralExpr of value:Value
+    | VariableExpr of name:Id
+    | OperationExpr of left_operand:Expression * operator:Operator * right_operand:Expression
+    | LambdaExpr of parameters:Id list * body:Scope
+    | AppExpr of arguments:Expression list * func:Expression
+    | FuncExpr of func_name:string * parameters:Id list * body:Scope
+    
+and Statement =
+    | Condition of condition:Expression * true_scope:Scope * false_scope:Scope option
+    | ConsoleWrite of message:Expression
+    | Assign of var_name:string * value:Expression
+    | While of condition:Expression * body:Scope
+    
+and Scope = Statement list
+and Closure = Closure of parameters:Id list * body:Scope * env:Environment 
+and Environment = Environment of context: Map<Id, Expression> * parent_context:Option<Environment>
+
+module Parser =
+    let ss = spaces // only for me
+    
+    // Numbers parsers
+    let pInt: Parser<Value, Unit> = pint32 .>> ss |>> Int
+    let pFloat: Parser<Value, Unit> = pfloat .>> ss |>> Float
+    
+    // boolean parser
+    let pBoolTrue = stringReturn "true" <| Bool(true) .>> ss
+    let pBoolFalse = stringReturn "false" <| Bool(false) .>> ss
+    let pBool: Parser<Value, Unit> =
+        choice [
+            pBoolTrue
+            pBoolFalse
+        ]
+    
+    // string parser
+    let pString: Parser<Value, Unit> =
+        (pchar '\"') >>. manyCharsTill anyChar (pchar '\"')
+        |>> (fun s ->
+            // printfn $"pString: %A{s}" // for debugging
+            Str s)
+        .>> ss
+
+    // common parser for all Value types
+    let pValue: Parser<Value, Unit> =
+        choice [
+            pFloat;
+            pInt;
+            pBool;
+            pString
+        ]     
+    
+    // for literal case
+    let pLiteralExpr: Parser<Expression, Unit> = pValue |>> LiteralExpr .>> ss
+    
+    // i want that variable can start with default char, @ or underscore
+    let validVarFirstChar c = Char.IsLetter(c) || c.Equals('@') || c.Equals('_')
+    
+    // for variable case
+    let pVariableExpr: Parser<Expression, Unit> = many1Satisfy2 validVarFirstChar Char.IsLetterOrDigit |>> VariableExpr .>> ss
+    
+    let pOperator =
+        choice [
+            stringReturn "+" Add
+            stringReturn "-" Subtract
+            stringReturn "*" Multiply
+            stringReturn "/" Divide
+            stringReturn "%" Mod
+
+            stringReturn ">" GreaterThan
+            stringReturn "<" LessThan
+            stringReturn ">=" GreaterThanOrEqual
+            stringReturn "<=" LessThanOrEqual
+            stringReturn "==" Equal
+            stringReturn "!=" NotEqual
+
+            // like python :D
+            stringReturn "and" And
+            stringReturn "or" Or
+        ] .>> ss
+    
+    // parsing left operand, operator, operand
+    let pBinaryExpr: Parser<Expression, Unit> =
+        pipe3
+            (pLiteralExpr <|> pVariableExpr)
+            pOperator
+            (pLiteralExpr <|> pVariableExpr)
+            (fun left op right -> OperationExpr (left, op, right))
+    
+    let pExpr =
+        choice [
+            attempt pBinaryExpr
+            pLiteralExpr;
+            pVariableExpr;
+        ]
+        
+    let pAssign: Parser<Statement, Unit> =
+        let identifier =
+            many1Satisfy2 validVarFirstChar Char.IsLetterOrDigit
+            .>> ss
+
+        pipe2
+            (identifier .>> pstring "=" .>> ss)
+            pExpr
+            (fun name expr -> Assign(name, expr))
+
+    let pStatement =
+        choice [
+            pAssign
+        ]
---
+++ src/Program/Program.fs
@@ -0,0 +1,81 @@
+﻿open Parser
+open FParsec
+
+let test parser strInput =
+    match run parser strInput with
+    | Success (result, _, _) -> printfn "%O" result
+    | Failure (error, _, _) -> printfn "%s" error
+
+// VALUE TEST
+
+printfn "Test 1: Parsing simple values\n"
+
+printfn "value: \"code1da sdas das dsada\""
+let x1_1 = test Parser.pString "\"code1da sdas das dsada\""
+
+printfn "value: true"
+let x1_2 = test Parser.pBool "true"
+
+printfn "value: false"
+let x1_3 = test Parser.pBool "false"
+
+printfn "value: 3.21312312321"
+let x1_4 = test Parser.pFloat "3.21312312321"
+
+printfn "value: 51312312"
+let x1_5 = test Parser.pInt "51312312"
+
+// LITERAL TEST
+
+printfn "\nTest 2: Parsing literal expressions\n"
+
+printfn "string literal: \"dsada\""
+let x2_1 = test Parser.pExpr "\"dsada\""
+
+printfn "bool literal: true"
+let x2_2 = test Parser.pExpr "true"
+
+printfn "bool literal: false"
+let x2_3 = test Parser.pExpr "false"
+
+printfn "int literal: 123123"
+let x2_4 = test Parser.pExpr "123123"
+
+printfn "float literal: 3.141231232131"
+let x2_5 = test Parser.pExpr "3.141231232131"
+
+printfn "\nTest 3: Parsing variable expressions\n"
+
+printfn "x variable"
+let x3_1 = test Parser.pExpr "x"
+
+printfn "xx1323123 variable"
+let x3_2 = test Parser.pExpr "x1323123"
+
+printfn "_x132 variable"
+let x3_3 = test Parser.pExpr "_x132"
+
+printfn "\nTest 4: Parsing binary expressions\n"
+
+printfn "case 1 + 3"
+let x4_1 = test Parser.pExpr "1 + 3"
+
+printfn "case 1 - x"
+let x4_2 = test Parser.pExpr "1 - x"
+
+printfn "case y / x"
+let x4_3 = test Parser.pExpr "y / x"
+
+printfn "case y and x"
+let x4_4 = test Parser.pExpr "y and x"
+
+printfn "\nTest 5: Parsing assignment\n"
+
+printfn "case 'x = 5'"
+let x5_1 = test Parser.pStatement "x = 5"
+
+printfn "case 'x = 1 + 5'"
+let x5_2 = test Parser.pStatement "x = 1 + 5"
+
+printfn "case 'x = y + z'"
+let x5_3 = test Parser.pStatement "x = y + z"
